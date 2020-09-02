f72bc784981e576223f3d42c97f5c60667e1b52b
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-02 16:01:53 +0300

    added ProductServiceImpl class

--- src/main/java/db/Storage.java
+++ src/main/java/db/Storage.java
@@ -6,11 +6,10 @@ import model.Product;
 
 public class Storage {
     public static final List<Product> products = new ArrayList<>();
-    private static long productId = 0;
+    private static Long productId = 0L;
 
     public static void addProduct(Product product) {
-        productId++;
-        product.setId(productId);
+        product.setId(++productId);
         products.add(product);
     }
 }
