5264d3131ba7ab6d75992416e1d0c01dd233d93c
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-08 11:32:59 +0300

    improved code style after merge

--- src/main/java/com/internet/shop/dao/OrderDao.java
+++ src/main/java/com/internet/shop/dao/OrderDao.java
@@ -1,12 +1,9 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.Order;
-import com.internet.shop.model.ShoppingCart;
 import java.util.List;
 
 public interface OrderDao extends GenericDao<Order, Long> {
-
-    Order completeOrder(ShoppingCart shoppingCart);
 
     List<Order> getUserOrders(Long userId);
 }
--- src/main/java/com/internet/shop/dao/ShoppingCartDao.java
+++ src/main/java/com/internet/shop/dao/ShoppingCartDao.java
@@ -1,15 +1,8 @@
 package com.internet.shop.dao;
 
-import com.internet.shop.model.Product;
 import com.internet.shop.model.ShoppingCart;
 
 public interface ShoppingCartDao extends GenericDao<ShoppingCart, Long> {
 
-    ShoppingCart addProduct(ShoppingCart shoppingCart, Product product);
-
-    boolean deleteProduct(ShoppingCart shoppingCart, Product product);
-
     boolean delete(ShoppingCart shoppingCart);
-
-    void clear(ShoppingCart shoppingCart);
 }
