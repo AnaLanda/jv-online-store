4246263712c8bc73296c5d57f772086da10ba425
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-15 16:14:22 +0300

    added AuthorizationFilter

---
+++ src/main/java/com/internet/shop/web/filters/AuthorizationFilter.java
@@ -0,0 +1,73 @@
+package com.internet.shop.web.filters;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.model.Role;
+import com.internet.shop.model.User;
+import com.internet.shop.service.UserService;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AuthorizationFilter implements Filter {
+    Map<String, List<Role.RoleName>> protectedUrls = new HashMap<>();
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private final UserService userService =
+            (UserService) injector.getInstance(UserService.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        protectedUrls.put("/users/all", List.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/order/complete", List.of(Role.RoleName.USER));
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String requestedUrl = req.getServletPath();
+        if (protectedUrls.get(requestedUrl) == null) {
+            filterChain.doFilter(req, resp);
+            return;
+        }
+        HttpSession session = req.getSession();
+        Long userId = (Long) session.getAttribute("user_id");
+        if (userId == null || userService.getById(userId) == null) {
+            resp.sendRedirect("/login");
+            return;
+        }
+        User user = userService.getById(userId);
+        if (isAuthorized(user, protectedUrls.get(requestedUrl))) {
+            filterChain.doFilter(req, resp);
+            return;
+        } else {
+            req.getRequestDispatcher("/WEB-INF/views/access-denied.jsp").forward(req, resp);
+            return;
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    private boolean isAuthorized(User user, List<Role.RoleName> authorizedRoles) {
+        for (Role.RoleName authorizedRole : authorizedRoles) {
+            for (Role userRole : user.getRoles()) {
+                if (authorizedRole.equals(userRole.getRoleName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/access-denied.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="style.jsp"%>
+<html>
+<head>
+    <title>Access Denied</title>
+</head>
+<body>
+<h3 style="color:#a9220e">You do not have access to this page.</h3>
+</body>
+</html>
