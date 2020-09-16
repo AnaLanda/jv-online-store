59a0ce8679d8c11850ea82035f8fe8dd4c6e4504
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-16 11:22:49 +0300

    improved code style

--- src/main/java/com/internet/shop/model/Role.java
+++ src/main/java/com/internet/shop/model/Role.java
@@ -4,9 +4,13 @@ public class Role {
     private Long id;
     private RoleName roleName;
 
-    private Role(RoleName roleName) { this.roleName = roleName; }
+    private Role(RoleName roleName) {
+        this.roleName = roleName;
+    }
 
-    public static Role of(String roleName) { return new Role(RoleName.valueOf(roleName)); }
+    public static Role of(String roleName) {
+        return new Role(RoleName.valueOf(roleName));
+    }
 
     public Long getId() {
         return id;
