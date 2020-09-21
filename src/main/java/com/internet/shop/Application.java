4ba2e6c559e08be513b3b4e8efb7a45c558b2fc7
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-21 12:13:12 +0300

    refactored code

--- src/main/java/com/internet/shop/Application.java
+++ src/main/java/com/internet/shop/Application.java
@@ -1,6 +1,6 @@
 package com.internet.shop;
 
-import com.internet.shop.dao.jdbc.ProductDaoJdbcImpl;
+import com.internet.shop.dao.ProductDao;
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Product;
 
@@ -8,19 +8,19 @@ public class Application {
     private static Injector injector = Injector.getInstance("com.internet.shop");
 
     public static void main(String[] args) {
-        ProductDaoJdbcImpl productDaoJdbc =
-                (ProductDaoJdbcImpl) injector.getInstance(ProductDaoJdbcImpl.class);
+        ProductDao productDao =
+                (ProductDao) injector.getInstance(ProductDao.class);
         Product product = new Product("Ukulele", 120.99);
         product.setId(1L);
         System.out.println("Added product to DB: "
-                + productDaoJdbc.create(product));
+                + productDao.create(product));
         System.out.println("Product retrieved from DB by ID: "
-                + productDaoJdbc.getById(1L));
+                + productDao.getById(1L));
         System.out.println("All products in DB: "
-                + productDaoJdbc.getAll());
+                + productDao.getAll());
         product.setName("Second-hand Ukulele, like new condition");
         product.setPrice(100);
-        System.out.println("Updated product :" + productDaoJdbc.update(product));
-        System.out.println("Deleted product: " + productDaoJdbc.deleteById(1L));
+        System.out.println("Updated product :" + productDao.update(product));
+        System.out.println("Deleted product: " + productDao.deleteById(1L));
     }
 }
