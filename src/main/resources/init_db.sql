d195e1d774a2680ebd6c513f7265648d3130b8f3
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-22 22:04:31 +0300

    fixed init_db sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `internet_shop`.`products` (
-  `product_id` BIGINT(11) NOT NULL,
+  `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `price` DECIMAL(7,2) NOT NULL,
   `deleted` TINYINT NOT NULL DEFAULT 0,
