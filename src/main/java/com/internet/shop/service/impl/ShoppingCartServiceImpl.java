d607708db55e8c8f31c240f34385c8bd1f793113
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-09-23 10:13:47 +0300

    improved code style

--- src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
@@ -26,7 +26,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public boolean deleteProduct(ShoppingCart shoppingCart, Product product) {
-        boolean result = shoppingCart.getProducts().removeIf(prod -> prod.getId().equals(product.getId()));
+        boolean result =
+                shoppingCart.getProducts().removeIf(prod -> prod.getId().equals(product.getId()));
         shoppingCartDao.update(shoppingCart);
         return result;
     }
